156e11aedbbf38814b4a84520d663511dd4a235c
Nemo <XZC168520@163.com>
2017-02-13 14:11:43 +0800

    Initial Commit

---
+++ ShowTestDome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShowTestDome
+//
+//  Created by Mjwon on 2017/2/13.
+//  Copyright © 2017年 Nemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
